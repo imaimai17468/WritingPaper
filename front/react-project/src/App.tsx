8212c4841d5cc0873a19a4c620af1472dc7eda37
Toshiki Imai <tosiki_801_life@outlook.jp>
2022-07-11 11:38:24 +0900

    add title and accordion

--- front/react-project/src/App.tsx
+++ front/react-project/src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.css';
+import { Box, Typography } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails,  } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Box component={'div'} sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    }}>
+      <Typography variant={'h2'}>WritingPaper</Typography>
+      <Accordion sx={{width: '60%'}}>
+      <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+    </Box>
   );
 }
 
