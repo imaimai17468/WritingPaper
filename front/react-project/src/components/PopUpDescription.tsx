680b67c0efaf86d3c39d19e636f8d1debd81815e
toshiki imai <tosiki_801_life@outlook.jp>
2022-07-14 14:05:11 +0900

    add styles

--- front/react-project/src/components/PopUpDescription.tsx
+++ front/react-project/src/components/PopUpDescription.tsx
@@ -12,7 +12,7 @@ export default function PopUpDescription(props: Props) {
     return(
         <Box component={'div'} sx={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
             <BasicPopover mainText={props.mainText} popText={props.popText} placement={'left'} />
-            <Typography sx={{px:2}}>{props.description}</Typography>
+            <Typography sx={{px:2}} color={'#E4DCCF'}>{props.description}</Typography>
         </Box>
     );
 }
--- front/react-project/src/pages/App.tsx
+++ front/react-project/src/pages/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
 import { TextField } from '@mui/material'
+import IconButton from '@mui/material/IconButton';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import PopUpDescription from '../components/PopUpDescription';
 import Keyboard from '../components/Keyboard';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Toolbar, AppBar } from '@mui/material';
+import InvertColorsIcon from '@mui/icons-material/InvertColors';
+import { style } from '@mui/system';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 function App() {
   const [inputText, setInputText] = useState<string>('');
   const [codeText, setCodeText] = useState<string>('');
-  
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
   return (
-    <div>
-      <Box component={'header'} sx={{height: '8vh', backgroundColor: '#1769aa', width: '100%', margin: 0}}>
-          <Typography variant="h4">WritingPaper</Typography>
-      </Box>
+    <Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              WritingPaper
+            </Typography>
+            <IconButton color='inherit' onClick={colorMode.toggleColorMode}>
+              <InvertColorsIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+    </Box>
       <Box component={'div'} sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+        display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
       }}>
         <Accordion sx={{my: 4}}>
         <AccordionSummary
@@ -28,7 +44,7 @@ function App() {
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography>Description</Typography>
+            <Typography color={'#E4DCCF'} >Description</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Box component={'div'} sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
@@ -47,10 +63,7 @@ function App() {
           <Box component={'div'} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2, width: '40%', minWidth: 300}}>
             <TextField label='input' value={inputText} multiline maxRows={5} fullWidth onChange={(event) => {setInputText(event.target.value)}}/>
             <TextField label='code' value={codeText} multiline minRows={15} maxRows={15} fullWidth onChange={(event) => {setCodeText(event.target.value)}}/>
-            <Box component={'div'} sx={{backgroundColor: '#1769aa', paddingBottom: 2, borderRadius: 5}}>
-              <Typography variant={'body1'} sx={{textAlign: 'center', py: 1, color:'whitesmoke'}}>Keyboard</Typography>
-              <Keyboard code={codeText} setCode={setCodeText} />
-            </Box>
+            <Keyboard code={codeText} setCode={setCodeText} />
           </Box>
           <Box component={'div'} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '40%', gap:2, minWidth: 300}}>
             <Button variant="contained">RUN</Button>
@@ -58,8 +71,61 @@ function App() {
           </Box>
         </Box>
       </Box>
-    </div>
+    </Box>
   );
 }
 
-export default App;
+export default function ToggleColorMode() {
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+  const theme = createTheme({
+    palette: {
+      mode,
+      ...(mode === 'light'
+        ? {
+            primary: {
+              main: '#576F72',
+              contrastText: '#E4DCCF',
+            },
+            divider: '#E4DCCF',
+            background: {
+              default: 'F0EBE3',
+              paper: '#7D9D9C',
+            },
+            text: {
+              primary: '#576F72',
+              secondary: '#576F72',
+            },
+          }
+        : {
+            primary: {
+              main: '#816797',
+              contrastText: '#D6D5A8',
+            },
+            divider: '#51557E',
+            background: {
+              default: '#1B2430',
+              paper: '#51557E',
+            },
+            text: {
+              primary: '#D6D5A8',
+              secondary: '#D6D5A8',
+            },
+          }
+        ),
+    },
+  })
+  const colorMode = {
+    toggleColorMode: () => {
+      setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+    },
+  };
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
